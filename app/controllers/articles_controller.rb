62cb583c0d03438ab1ab36ff0b4d42b9cdcb3f00
Paulo Perez <pauloperez@bittronic.net>
2016-02-15 09:16:22 -0500

    link editar y ver con sus metodos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,40 @@
 class ArticlesController < ApplicationController
-def index
-   @articulo = Article.all
-end
+  def index
+     @articulo = Article.all
+  end
   def new
-
+    @article = Article.new
   end
 
   def create
     #render plain: params[:article].inspect
-    @articulo = Article.new(article_params)
-    @articulo.save
-    redirect_to @articulo
+    @article = Article.new(article_params)
+    if @article.save
+        redirect_to @article
+    else
+        render 'new'
+    end
+
   end
+
   def show
-    @articulo = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
+
+  def edit
+    @article= Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+  end
+
+end
   private
     def article_params
       params.require(:article).permit(:title, :text)
